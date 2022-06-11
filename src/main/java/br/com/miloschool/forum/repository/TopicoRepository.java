74fe176d10c5bf6529b9159e061195b0d5fed89b
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-06-11 15:58:59 -0300

    update TopicoRepository class

--- src/main/java/br/com/miloschool/forum/repository/TopicoRepository.java
+++ src/main/java/br/com/miloschool/forum/repository/TopicoRepository.java
@@ -1,9 +1,13 @@
 package br.com.miloschool.forum.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.miloschool.forum.modelo.Topico;
 
 public interface TopicoRepository extends JpaRepository<Topico, Long> {
+
+  List<Topico> findByCursoNome(String nomeCurso);
 
 }
