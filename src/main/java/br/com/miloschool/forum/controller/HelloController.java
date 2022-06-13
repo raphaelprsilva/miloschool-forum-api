e3c32b2db94914fcff5a6ccb26bc4c28b8bf54ec
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-06-13 08:10:40 -0300

    implements detailed response from id

--- src/main/java/br/com/miloschool/forum/controller/HelloController.java
+++ src/main/java/br/com/miloschool/forum/controller/HelloController.java
@@ -1,5 +1,15 @@
 package br.com.miloschool.forum.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class HelloController {
 
+  @RequestMapping("/")
+  @ResponseBody
+  public String hello() {
+    return "hello world";
+  }
 }
